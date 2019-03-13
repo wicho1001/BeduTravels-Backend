6f77375549fae7cc475fe48da86d92029962866c
Jose Guerrero <wichoguerrero@yahoo.com>
2019-03-13 14:06:45 -0600

    fixing port

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+// Express
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
-const PORT = 3000
-
+const APP_PORT = process.env.APP_PORT || 3000
+// Dependecies y/o modelos
+const createError = require('http-errors')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-
 const TourRoutes = require('./routes/Tours')
 
+// conexion a base de datos 
 mongoose.connect('mongodb://localhost:27017/bedutravels', { useNewUrlParser: true }, () => console.log('Conectado a BeduTravelsDB'))
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// use
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500
+  return res.status(statusCode).json({
+    code: statusCode,
+    message: err.message
+  })
+})
 
 app.use('/tours', TourRoutes)
 
-app.listen(PORT, () => console.log(`Escuchando en el http://localhost:${PORT}`))
+// listen
+app.listen(APP_PORT, () => console.log(`Escuchando en el http://localhost:${APP_PORT}`))
