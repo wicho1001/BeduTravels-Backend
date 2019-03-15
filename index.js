e9a28e9fec9b0604b39ee06a7ad483b8f32861e5
Jose Guerrero <wichoguerrero@yahoo.com>
2019-03-14 18:18:32 -0600

    add mongoose connection

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const APP_PORT = process.env.APP_PORT || 3000
+
 // Dependecies y/o modelos
+
 const createError = require('http-errors')
 const mongoose = require('mongoose')
 const TourRoutes = require('./routes/Tours')
+const APP_MONGO_URI = process.env.APP_MONGO_URI
 
-// conexion a base de datos 
-mongoose.connect('mongodb://localhost:27017/bedutravels', { useNewUrlParser: true }, () => console.log('Conectado a BeduTravelsDB'))
+// conexion mongoose
+
+mongoose.connect(APP_MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log(`Connect to ${APP_MONGO_URI}`))
+  .catch((err) => { throw err })
 
 // use
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -30,4 +37,5 @@ app.use((err, req, res, next) => {
 app.use('/tours', TourRoutes)
 
 // listen
+
 app.listen(APP_PORT, () => console.log(`Escuchando en el http://localhost:${APP_PORT}`))
