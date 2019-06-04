008988c55dee1d2e8eb43419b5bee67129128e3c
wicho1001 <wichoguerrero@yahoo.com>
2019-06-04 13:16:54 -0500

    minor fixes

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 // Express
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const APP_PORT = process.env.APP_PORT || 3000
+const APP_PORT = 3000
 
 // Dependecies y/o modelos
-
 const cors = require('cors')
-const createError = require('http-errors')
 const mongoose = require('mongoose')
 const TourRoutes = require('./routes/Tours')
-const APP_MONGO_URI = process.env.APP_MONGO_URI
-// conexion mongoose
+const APP_MONGO_URI = 'mongodb+srv://userAdmin:*secret00*@bedutravels-b1trr.mongodb.net/BeduTravels?retryWrites=true&w=majority'
 
+// conexion mongoose
 mongoose.connect(APP_MONGO_URI, { useNewUrlParser: true })
-  .then(() => console.log(`Connect to ${APP_MONGO_URI}`))
+  .then(() => console.log(`Connect to MongoAtlas`))
   .catch((err) => { throw err })
 
 // use
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
@@ -29,24 +25,8 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Request-Method: POST')
   next()
 })
-// app.use((req, res, next) => {
-//   next(createError(404))
-// })
-
-// app.use((err, req, res, next) => {
-//   const statusCode = err.status || 500
-//   return res.status(statusCode).json({
-//     code: statusCode,
-//     message: err.message
-//   })
-// })
 
 app.use('/tours', TourRoutes)
 
-app.get('/armando', (req, res, next) => {
-  res.send(console.log(req))
-})
-
 // listen
-
 app.listen(APP_PORT, () => console.log(`Escuchando en el http://localhost:${APP_PORT}`))
